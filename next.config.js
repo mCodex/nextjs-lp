ff99c1084ff9217f1727019c7ab5044c6d07a8a7
Mateus Andrade <mat.andrade@live.com>
2019-05-01 19:03:31 -0300

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const withCSS = require('@zeit/next-css'); //eslint-disable-line
+
+module.exports = {
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    };
+    return config;
+  }
+};
+
+module.exports = withCSS({
+  target: 'serverless',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+          name: '[name].[ext]'
+        }
+      }
+    });
+    return config;
+  }
+});
